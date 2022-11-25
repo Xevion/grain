6ceba5fc83524822fd54a364548092d79ed28d51
Xevion <ryanchwalters@gmail.com>
2022-11-25 02:33:45 -0600

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default ({mode }) => {
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
   return defineConfig({
-    base: process.env.NODE_ENV == 'production' ? '/grain' : '',
+    base: process.env.NODE_ENV == 'production' ? '/grain/' : '/',
     plugins: [react(), tsconfigPaths()],
   })
 }
